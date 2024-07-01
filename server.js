afc50c76ba1a38e34f8d1f64b13865a747c69b4b
PayalAhirwar <payalahirwar03@gmail.com>
2024-07-02 01:03:46 +0530

    modified

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const cors = require('cors');
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 const userRoute = require("./routes/userRoutes");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorRoute");
 
+
+app.use(cors({
+    origin: 'https://6683021e1d543f82306c4c97--silly-cupcake-53f9be.netlify.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use('/api/user', userRoute);
-app.use('/api/admin',adminRoute);
-app.use('/api/doctor',doctorRoute);
+app.use('/api/admin', adminRoute);
+app.use('/api/doctor', doctorRoute);
 
 const port = process.env.PORT || 5000;
 
